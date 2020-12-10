d37587a1544c445948a6e3028a48af82651af81e
Sumsky <18373582@buaa.edu.cn>
2020-12-10 17:43:41 +0800

    Add testapi.py for api testing

---
+++ testapi.py
@@ -0,0 +1,17 @@
+# 用于测试flaskAPI接口
+import requests
+import json
+import time
+
+data = {
+    'task': 0,
+    'q': '我喜欢三月的狮子这种剧情向的'
+    # 'qtype': {'args': {'JOJO的奇妙冒险 星尘斗士 埃及篇': ['anime'], 'Joao': ['staff']}, 'feature_types': ['others']},
+    # 'anime_list': ['JOJO的奇妙冒险 星尘斗士 埃及篇']
+}
+r = requests.post(
+    url='https://sumsky.xyz:5000/api',
+    json=json.dumps(data)
+)
+print(json.loads(r.text))   # 响应内容
+print(r.elapsed.total_seconds())    # 响应时间
